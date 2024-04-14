cd8b8aabc97e4d01d41030ac1211feae2a1461d3
Govind <govindlohar3210@gamil.com>
2024-04-14 18:42:56 +0530

    env file added and last

--- BackendFood/app.js
+++ BackendFood/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 // const DATABASE_URL=process.env.DATABASE_URL;
-const DATABASE_URL = "mongodb://Govind_Lohar:GOVIND_LOHAR_3210@ac-v9eezp9-shard-00-00.40qcro8.mongodb.net:27017,ac-v9eezp9-shard-00-01.40qcro8.mongodb.net:27017,ac-v9eezp9-shard-00-02.40qcro8.mongodb.net:27017/FoodDelevery?ssl=true&replicaSet=atlas-piid7l-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+const DATABASE_URL =process.env.MONGODB_URL;
 
 //parse middleware
 app.use(express.json());
